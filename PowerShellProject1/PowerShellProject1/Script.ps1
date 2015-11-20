f0b783cd81196f732aaf0f33dcd935beda98d458
Adam Kufel <adam.kufel@onlinesolutions.pl>
2015-11-20 13:37:36 +0100

    changed script1 and added script2

--- PowerShellProject1/PowerShellProject1/Script.ps1
+++ PowerShellProject1/PowerShellProject1/Script.ps1
@@ -2,3 +2,5 @@
 # Script.ps1
 #
 "Hello world"
+"this is created on scrip.ps1 file in dev branch"
+"this is created and not saved"
---
+++ PowerShellProject1/PowerShellProject1/script2.ps1
@@ -0,0 +1,4 @@
+#
+# Script1.ps1
+#
+"this is second script from dev branch"
